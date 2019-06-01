680fd34f993e8a63596c6a3b799ed17a66f5cc11
vivek bharda <vivekkbharda@gmail.com>
2019-06-01 17:39:22 +0530

    Updates

---
+++ precache-manifest.cc6dbe6061638f9e424c313dfab68a95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46237b966b2b5c94435c7f13ee1b249f",
+    "url": "/stock-price/index.html"
+  },
+  {
+    "revision": "708a421ecfa3a72c64d0",
+    "url": "/stock-price/static/css/main.aec09047.chunk.css"
+  },
+  {
+    "revision": "0f218f35e0aae1b54d0d",
+    "url": "/stock-price/static/js/2.10b4bc55.chunk.js"
+  },
+  {
+    "revision": "708a421ecfa3a72c64d0",
+    "url": "/stock-price/static/js/main.598f5579.chunk.js"
+  },
+  {
+    "revision": "f2c2eec386b3ab8b43ed",
+    "url": "/stock-price/static/js/runtime~main.a6ac7259.js"
+  },
+  {
+    "revision": "de08db182391e4f66c7833a61bbdba82",
+    "url": "/stock-price/static/media/bgimage.de08db18.jpg"
+  }
+]);
